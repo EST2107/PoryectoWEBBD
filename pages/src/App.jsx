25dd4d4a93408135721d3e4dabc981c74b81f040
Edmundo Aguilar <edmundo2004.ea@gmail.com>
2024-03-30 02:10:55 -0600

    creando proyecto react

---
+++ pages/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavbarComp() {
+    return (
+      <>
+        <Navbar bg="light" expand="lg" className="justify-content-center">
+          <Container>
+            <Navbar.Brand href="http://localhost:3000/">Delicias Dilcia</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="me-auto">
+              <Nav.Link href=''>Clientes</Nav.Link>
+              <Nav.Link href=''>platillos</Nav.Link>
+              <Nav.Link href=''>Productos</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+    );
+  
+}
+
+export default NavbarComp
