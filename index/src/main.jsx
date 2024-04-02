d536628e03bb060d4e8278cca0e15e9c693173f7
Edmundo Aguilar <edmundo2004.ea@gmail.com>
2024-04-02 07:42:24 -0600

    index, kinda

---
+++ index/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {IndexC} from './index'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IndexC></IndexC>
+  </React.StrictMode>,
+)
